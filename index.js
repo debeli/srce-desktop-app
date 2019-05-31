5cc9d3a9afe679d5c95baeec59a666e028c8e1e0
debeli <djolex@gmail.com>
2019-05-31 20:42:34 +0200

    Revert "Fork sync"

--- index.js
+++ index.js
@@ -1,57 +1,12 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const { app, BrowserWindow, Menu } = require('electron');
-const DBHelper = require('./src/database/dbHelper');
+const { app, BrowserWindow } = require("electron");
 
 let window;
-
 function createWindow() {
-    // Menu
-    const menu = Menu.buildFromTemplate([
-        {
-            label: 'Main',
-            submenu: [
-                {
-                    label: 'Exit',
-                    click() {
-                        app.quit();
-                    }
-                }
-            ]
-        },
-        {
-            label: 'Database',
-            submenu: [
-                {
-                    label: 'Delete Database',
-                    click() {
-                        DBHelper.DeleteDatabae();
-                    }
-                },
-                {
-                    label: 'Add person',
-                    click() {
-                        DBHelper.addPersonToDb();
-                    }
-                }
-            ]
-        }
-    ]);
-
-    Menu.setApplicationMenu(menu);
-
-    window = new BrowserWindow({
-        width: 640,
-        height: 480
-    });
-    window.loadFile('index.html');
-    window.on('closed', () => {
-        window = null;
-    });
+  window = new BrowserWindow({ width: 640, height: 480 });
+  window.loadFile("index.html");
+  window.on("closed", () => {
+    window = null;
+  });
 }
 
-function databaseOperations() {
-    DBHelper.databaseAutomation();
-}
-
-app.on('ready', createWindow);
-app.on('ready', databaseOperations);
+app.on("ready", createWindow);
